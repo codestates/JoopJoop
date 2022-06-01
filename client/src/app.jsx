181e125650f5f7de1df9ba151f9c969193ed6fdc
woobaeh <qozkth@gmail.com>
2022-06-02 01:35:50 +0900

    add navbar components

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,53 +1,29 @@
 import React from 'react';
-import { BrowserRouter, Route, Link, Switch } from 'react-router-dom';
-import Chat from './pages/chat';
-import Community from './pages/community';
-import Home from './pages/home';
-import Profile from './pages/profile';
-import Schedule from './pages/schedule';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Chat from './components/pages/chat';
+import Community from './components/pages/community';
+import Home from './components/pages/home';
+import Logout from './components/pages/logout';
+import Mypage from './components/pages/mypage';
+import Schedule from './components/pages/schedule';
 import './app.css';
+import Navbar from './components/navbar';
 
 function App() {
   return (
-    <BrowserRouter>
-      <nav id='navbar'>
-        <ul className='navbar__menu'>
-          <li>
-            <Link to='/'>홈</Link>
-          </li>
-          <li>
-            <Link to='/schedule'>일정</Link>
-          </li>
-          <li>
-            <Link to='/chat'>채팅</Link>
-          </li>
-          <li>
-            <Link to='/community'>커뮤니티</Link>
-          </li>
-        </ul>
-        <div className='navbar__mypage'>
-          <i class='fa-solid fa-bell'></i>
-          <Link to='/profile'>Profile</Link>
-        </div>
-      </nav>
-      <Switch>
-        <Route path={['/', '/home']} exact>
-          <Home />
-        </Route>
-        <Route path='/schedule'>
-          <Schedule />
-        </Route>
-        <Route path='/chat'>
-          <Chat />
-        </Route>
-        <Route path='/community'>
-          <Community />
-        </Route>
-        <Route path='/profile'>
-          <Profile />
-        </Route>
-      </Switch>
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <Switch>
+          <Route path={['/', '/home']} exact component={Home} />
+          <Route path='/schedule' component={Schedule} />
+          <Route path='/chat' component={Chat} />
+          <Route path='/community' component={Community} />
+          <Route path='/mypage' component={Mypage} />
+          <Route path='/logout' component={Logout} />
+        </Switch>
+      </BrowserRouter>
+    </>
   );
 }
 
