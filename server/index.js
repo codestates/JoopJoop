122f73b1ac5f41291f9d940d69e88b9e5f0f3867
L9628 <mhp4718@gmail.com>
2022-06-05 23:43:34 +0900

    PR을 위한 Commit

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
@@ -14,6 +15,13 @@ const { swaggerUi, specs } = require('./modules/swagger');
 
 dotenv.config();
 app.use(express.json());
+app.use(
+  cors({
+    origin: ['http://localhost:3000', 'http://localhost:5000'],
+    credentials: true,
+    method: ['GET', 'POST', 'DELETE', 'PUT'],
+  })
+);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
 mongoose
   .connect(
