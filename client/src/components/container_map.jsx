850071299aee6cbb539be1ed06ec80ed0ae2b230
meanjeful <meanjeful@gmail.com>
2022-06-15 20:50:41 +0900

    fix create gather logic

--- client/src/components/container_map.jsx
+++ client/src/components/container_map.jsx
@@ -11,14 +11,15 @@ const MapContainer = props => {
     };
     const map = new kakao.maps.Map(container, options);
 
-    var markerPosition = new kakao.maps.LatLng(props.latitude, props.longitude);
+    const markerPosition = new kakao.maps.LatLng(
+      props.latitude,
+      props.longitude,
+    );
 
-    // 마커를 생성합니다
-    var marker = new kakao.maps.Marker({
+    const marker = new kakao.maps.Marker({
       position: markerPosition,
     });
 
-    // 마커가 지도 위에 표시되도록 설정합니다
     marker.setMap(map);
 
     let zoomControl = new kakao.maps.ZoomControl();
