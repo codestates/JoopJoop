2c44964a20aeaa8fa0b622e52b54589a06d8be4b
lasags <lasags@naver.com>
2022-06-16 19:29:25 +0900

    fix double_quote_final

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,26 +1,26 @@
-import React, { useEffect, useState } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Navbar from './components/navbar';
-import Chat from './pages/chat';
-import Community from './pages/community';
-import Home from './pages/home';
-import Schedule from './pages/schedule';
-import Footer from './components/footer';
-import Landing from './pages/landing';
-import './index.css';
-import Dropdown from './components/dropdown';
-import axios from 'axios';
-import { connect } from 'react-redux';
-import action from './redux/action';
-import Mypage from './pages/mypage';
+import React, { useEffect, useState } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Navbar from "./components/navbar";
+import Chat from "./pages/chat";
+import Community from "./pages/community";
+import Home from "./pages/home";
+import Schedule from "./pages/schedule";
+import Footer from "./components/footer";
+import Landing from "./pages/landing";
+import "./index.css";
+import Dropdown from "./components/dropdown";
+import axios from "axios";
+import { connect } from "react-redux";
+import action from "./redux/action";
+import Mypage from "./pages/mypage";
 
-const mapStateToProps = state => {
+const mapStateToProps = (state) => {
   return {
     isLogin: state.isLogin,
   };
 };
 
-const mapDispatchToProps = dispatch => {
+const mapDispatchToProps = (dispatch) => {
   return {
     setIsLogin: (boolean) => dispatch(action.setIsLogin(boolean)),
   };
@@ -28,54 +28,42 @@ const mapDispatchToProps = dispatch => {
 
 function App({ isLogin, setIsLogin }) {
   const onLogin = (email, password) => {
-    console.log('로그인요청');
+    console.log("로그인요청");
     const data = {
       email,
       password,
     };
     axios
-<<<<<<< HEAD
       .post(process.env.REACT_APP_LOCALSERVER_URL + "/auth/login", data, {
-=======
-      .post(process.env.REACT_APP_LOCALSERVER_URL + '/auth/login', data, {
->>>>>>> 63216d85 (fix double_quote)
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         withCredentials: true,
         HttpOnly: true,
-        samesite: 'Secure',
+        samesite: "Secure",
       })
-      .then(res => {
+      .then((res) => {
         onLoginSuccess(res);
       })
       .catch((error) => {
-        console.log('onLogin 함수');
+        console.log("onLogin 함수");
       });
   };
 
-  const onLogout = e => {
+  const onLogout = (e) => {
     axios
-<<<<<<< HEAD
       .get(process.env.REACT_APP_LOCALSERVER_URL + "/auth/logout", {
-=======
-      .get(process.env.REACT_APP_LOCALSERVER_URL + '/auth/logout', {
->>>>>>> 63216d85 (fix double_quote)
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         withCredentials: true,
         HttpOnly: true,
-        samesite: 'Secure',
+        samesite: "Secure",
       })
       .then((res) => {
-<<<<<<< HEAD
-        console.log("로그아웃 완료");
-=======
->>>>>>> 63216d85 (fix double_quote)
         setIsLogin(false);
       })
-      .catch(err => {
+      .catch((err) => {
         console.error(err);
       });
   };
@@ -84,74 +72,36 @@ function App({ isLogin, setIsLogin }) {
     console.log(process.env.REACT_APP_LOCALSERVER_URL);
     axios
       .post(
-<<<<<<< HEAD
         process.env.REACT_APP_LOCALSERVER_URL + "/auth/refresh",
         { data: "refresh" },
-=======
-        process.env.REACT_APP_LOCALSERVER_URL + '/auth/refresh',
-        { data: 'refresh' },
->>>>>>> 63216d85 (fix double_quote)
         {
           withCredentials: true,
-        },
+        }
       )
       .then((res) => {
         console.log(res);
         onLoginSuccess(res);
-        console.log('resfresh 성공');
+        console.log("resfresh 성공");
       })
       .catch((error) => {
-<<<<<<< HEAD
         console.log("refresh 실패");
-=======
-        console.log('refresh 실패');
->>>>>>> 63216d85 (fix double_quote)
         setIsLogin(false);
       });
   };
 
-<<<<<<< HEAD
-  const onLoginSuccess = res => {
-    const { accessToken, email, nickname, _id, profileImg } = res.data;
-
-    getGatherings();
-=======
   const onLoginSuccess = (res) => {
     const { accessToken } = res.data;
     // console.log("onloginsuccess");
     // console.log(accessToken);
     //login state true
->>>>>>> 63216d85 (fix double_quote)
     setIsLogin(true);
     // accessToken 설정
-    axios.defaults.headers.common['token'] = accessToken;
+    axios.defaults.headers.common["token"] = accessToken;
     // accessToken 만료하기 1분 전에 로그인 연장
     // setTimeout(onSilentRefresh, JWT_EXPIRRY_TIME - 60000);
     // getGatherings(accessToken);
   };
 
-<<<<<<< HEAD
-  const getGatherings = () => {
-    axios
-      .get(process.env.REACT_APP_LOCALSERVER_URL + "/gatherings", {
-        withCredentials: true,
-      })
-      .then(data => {
-        data.data
-          .filter(gathering => gathering.author !== null)
-          .forEach(
-            gathering =>
-              (gathering.author.profileImg =
-                process.env.REACT_APP_LOCALSERVER_URL +
-                "/" +
-                gathering.author.profileImg),
-          );
-        setGatherings([...data.data]);
-      });
-  };
-  useEffect(() => {
-    setIsLogin(false);
-=======
   //! gatherings 정보가져오기, 분리 필요
   // const getGatherings = () => {
   //   axios
@@ -163,7 +113,6 @@ function App({ isLogin, setIsLogin }) {
   // };
 
   const componentDidMount = () => {
->>>>>>> 63216d85 (fix double_quote)
     onSilentRefresh();
   };
   componentDidMount();
@@ -178,12 +127,12 @@ function App({ isLogin, setIsLogin }) {
     const hideMenu = () => {
       if (window.innerWidth > 768 && isOpen) {
         setIsOpen(false);
-        console.log('i resized');
+        console.log("i resized");
       }
     };
-    window.addEventListener('resize', hideMenu);
+    window.addEventListener("resize", hideMenu);
     return () => {
-      window.removeEventListener('resize', hideMenu);
+      window.removeEventListener("resize", hideMenu);
     };
   });
 
@@ -194,12 +143,12 @@ function App({ isLogin, setIsLogin }) {
         {isLogin ? <Navbar toggle={toggle} logout={onLogout} /> : null}
         {isLogin ? (
           <Switch>
-            <Route path='/' exact component={Landing} />
-            <Route path='/home' component={Home} />
-            <Route path='/schedule' component={Schedule} />
-            <Route path='/chat' component={Chat} />
-            <Route path='/community' component={Community} />
-            <Route path='/mypage' component={Mypage} />
+            <Route path="/" exact component={Landing} />
+            <Route path="/home" component={Home} />
+            <Route path="/schedule" component={Schedule} />
+            <Route path="/chat" component={Chat} />
+            <Route path="/community" component={Community} />
+            <Route path="/mypage" component={Mypage} />
           </Switch>
         ) : (
           <Landing onLogin={onLogin} />
--- client/src/modals/modalSignUp.jsx
+++ client/src/modals/modalSignUp.jsx
@@ -1,17 +1,17 @@
-import React, { useState, useRef } from 'react';
-import ReactDom from 'react-dom';
-import axios from 'axios';
-import Button from '../components/button';
-import logo from '../img/Logo.png';
-import { XIcon } from '@heroicons/react/solid';
-import { useForm } from 'react-hook-form';
-import { lightFormat } from 'date-fns';
-import KakaoLogin from 'react-kakao-login';
+import React, { useState, useRef } from "react";
+import ReactDom from "react-dom";
+import axios from "axios";
+import Button from "../components/button";
+import logo from "../img/Logo.png";
+import { XIcon } from "@heroicons/react/solid";
+import { useForm } from "react-hook-form";
+import { lightFormat } from "date-fns";
+import KakaoLogin from "react-kakao-login";
 
-const localURL = 'http://localhost:5000';
+const localURL = "http://localhost:5000";
 
 const ModalSignUp = ({ modalOpen, closeModal }) => {
-  const [verifyNumber, setVerifyNumber] = useState('');
+  const [verifyNumber, setVerifyNumber] = useState("");
 
   const {
     register,
@@ -21,7 +21,7 @@ const ModalSignUp = ({ modalOpen, closeModal }) => {
     formState: { errors },
   } = useForm();
   const password = useRef();
-  password.current = watch('password');
+  password.current = watch("password");
 
   const onSubmit = (data) => {
     const { email, password, nickname } = data;
@@ -36,12 +36,12 @@ const ModalSignUp = ({ modalOpen, closeModal }) => {
           nickname: nickname,
         },
         {
-          headers: { 'Content-Type': 'application/json' },
+          headers: { "Content-Type": "application/json" },
           withCredentials: true,
         }
       )
       .then((res) => {
-        alert('회원가입 되었습니다! 로그인하세요');
+        alert("회원가입 되었습니다! 로그인하세요");
         closeModal();
       });
   };
@@ -54,7 +54,7 @@ const ModalSignUp = ({ modalOpen, closeModal }) => {
           email: email,
         },
         {
-          headers: { 'Content-Type': 'application/json' },
+          headers: { "Content-Type": "application/json" },
           withCredentials: true,
         }
       )
@@ -67,60 +67,60 @@ const ModalSignUp = ({ modalOpen, closeModal }) => {
 
   if (!modalOpen) return null;
   return ReactDom.createPortal(
-    <div className='container-modal'>
-      <div className='modal-normal gap-3' onSubmit={(e) => e.preventDefault()}>
-        <div className='relative w-full'>
+    <div className="container-modal">
+      <div className="modal-normal gap-3" onSubmit={(e) => e.preventDefault()}>
+        <div className="relative w-full">
           <button
-            className='absolute left-[91.5%] bottom-2'
+            className="absolute left-[91.5%] bottom-2"
             onClick={closeModal}
           >
-            <XIcon className='h-5 w-5' />
+            <XIcon className="h-5 w-5" />
           </button>
         </div>
-        <img className='w-52 my-3' src={logo} alt='err'></img>
+        <img className="w-52 my-3" src={logo} alt="err"></img>
         <form
-          className='flex flex-col justify-center text-center items-center'
+          className="flex flex-col justify-center text-center items-center"
           onSubmit={handleSubmit(onSubmit)}
         >
           <input
-            name='email'
-            type='email'
-            className='w-72  h-10 bg-white text-center rounded-3xl outline md:outline-2 placeholder:text-grey-70'
-            placeholder='Email을 입력하세요.'
-            {...register('email', { required: true, pattern: /^\S+@\S+$/i })}
+            name="email"
+            type="email"
+            className="w-72  h-10 bg-white text-center rounded-3xl outline md:outline-2 placeholder:text-grey-70"
+            placeholder="Email을 입력하세요."
+            {...register("email", { required: true, pattern: /^\S+@\S+$/i })}
           />
           {errors.email && (
-            <p className='text-xs text-red'> 필수 입력 사항입니다.</p>
+            <p className="text-xs text-red"> 필수 입력 사항입니다.</p>
           )}
           <button
-            type='button'
+            type="button"
             onClick={() => {
-              const email = getValues('email');
+              const email = getValues("email");
               verifyEmail(email);
             }}
           >
             이메일 인증
           </button>
           <input
-            name='verifyNumber'
-            type='password'
-            className='w-30 h-6   bg-white text-center rounded-3xl my-3 outline md:outline-2 placeholder:text-grey-70'
-            placeholder='인증번호를 입력하세요.'
-            {...register('verifyNumber', { required: true })}
+            name="verifyNumber"
+            type="password"
+            className="w-30 h-6   bg-white text-center rounded-3xl my-3 outline md:outline-2 placeholder:text-grey-70"
+            placeholder="인증번호를 입력하세요."
+            {...register("verifyNumber", { required: true })}
           />
           {errors.verifyNumber && (
-            <p className='text-xs text-red'> 필수 입력 사항입니다.</p>
+            <p className="text-xs text-red"> 필수 입력 사항입니다.</p>
           )}
           <button
-            type='button'
+            type="button"
             onClick={() => {
-              const inputVerifyNumber = getValues('verifyNumber');
+              const inputVerifyNumber = getValues("verifyNumber");
               if (inputVerifyNumber === verifyNumber) {
-                const target = document.querySelector('.verified');
+                const target = document.querySelector(".verified");
                 target.disabled = false;
-                alert('인증 번호가 일치합니다. 계속해서 회원가입 진행해주세요');
+                alert("인증 번호가 일치합니다. 계속해서 회원가입 진행해주세요");
               } else {
-                alert('인증번호를 다시 확인해주세요');
+                alert("인증번호를 다시 확인해주세요");
               }
             }}
           >
@@ -128,61 +128,61 @@ const ModalSignUp = ({ modalOpen, closeModal }) => {
           </button>
           <input
             disabled
-            name='password'
-            type='password'
-            className='verified w-367 h-10   bg-white text-center rounded-3xl my-3 outline md:outline-2 placeholder:text-grey-70'
-            placeholder='비밀번호를 입력하세요.'
-            {...register('password', { required: true, minLength: 6 })}
+            name="password"
+            type="password"
+            className="verified w-367 h-10   bg-white text-center rounded-3xl my-3 outline md:outline-2 placeholder:text-grey-70"
+            placeholder="비밀번호를 입력하세요."
+            {...register("password", { required: true, minLength: 6 })}
           />
-          {errors.password && errors.password.type === 'required' && (
-            <p className='text-xs text-red'>필수 입력 사항입니다.</p>
+          {errors.password && errors.password.type === "required" && (
+            <p className="text-xs text-red">필수 입력 사항입니다.</p>
           )}
-          {errors.password && errors.password.type === 'minLength' && (
-            <p className='text-xs text-red'>
+          {errors.password && errors.password.type === "minLength" && (
+            <p className="text-xs text-red">
               비밀번호는 6글자 이상이어야 합니다.
             </p>
           )}
           <input
-            name='passwordConfirm'
-            type='password'
-            className=' w-367 h-10  bg-white text-center rounded-3xl mb-3 outline md:outline-2 placeholder:text-grey-70'
-            placeholder='비밀번호를 다시 입력하세요.'
-            {...register('passwordConfirm', {
+            name="passwordConfirm"
+            type="password"
+            className=" w-367 h-10  bg-white text-center rounded-3xl mb-3 outline md:outline-2 placeholder:text-grey-70"
+            placeholder="비밀번호를 다시 입력하세요."
+            {...register("passwordConfirm", {
               required: true,
               validate: (value) => value === password.current,
             })}
           />
           {errors.passwordConfirm &&
-            errors.passwordConfirm.type === 'required' && (
-              <p className='text-xs text-red'>필수 입력 사항입니다.</p>
+            errors.passwordConfirm.type === "required" && (
+              <p className="text-xs text-red">필수 입력 사항입니다.</p>
             )}
           {errors.passwordConfirm &&
-            errors.passwordConfirm.type === 'validate' && (
-              <p className='text-xs text-red'>비밀번호가 일치하지 않습니다.</p>
+            errors.passwordConfirm.type === "validate" && (
+              <p className="text-xs text-red">비밀번호가 일치하지 않습니다.</p>
             )}
           <input
-            name='nickname'
-            type='text'
-            className=' w-72  h-10 bg-white text-center rounded-3xl outline md:outline-2 placeholder:text-grey-70'
-            placeholder='닉네임을 입력하세요.'
-            {...register('nickname', { required: true, maxLength: 10 })}
+            name="nickname"
+            type="text"
+            className=" w-72  h-10 bg-white text-center rounded-3xl outline md:outline-2 placeholder:text-grey-70"
+            placeholder="닉네임을 입력하세요."
+            {...register("nickname", { required: true, maxLength: 10 })}
           />
-          {errors.nickname && errors.nickname.type === 'required' && (
-            <p className='text-xs text-red'>필수 입력 사항입니다.</p>
+          {errors.nickname && errors.nickname.type === "required" && (
+            <p className="text-xs text-red">필수 입력 사항입니다.</p>
           )}
-          {errors.nickname && errors.nickname.type === 'maxLength' && (
-            <p className='text-xs text-red'>최대 10글자 입니다.</p>
+          {errors.nickname && errors.nickname.type === "maxLength" && (
+            <p className="text-xs text-red">최대 10글자 입니다.</p>
           )}
           <input
-            type='submit'
-            value='회원가입'
-            className='w-36 h-12 btn-green mx-3 my-3 text-center rounded-3xl  text-white'
+            type="submit"
+            value="회원가입"
+            className="w-36 h-12 btn-green mx-3 my-3 text-center rounded-3xl  text-white"
             onClick={onSubmit}
           />
         </form>
       </div>
     </div>,
-    document.getElementById('modal')
+    document.getElementById("modal")
   );
 };
 
