d820c38faa84ecfc4a0cd73c943ec690d21b33d8
L9628 <mhp4718@gmail.com>
2022-06-18 18:13:00 +0900

    edit server/passport/kakao 주석, 콘솔로그 처리

--- server/passport/kakao.js
+++ server/passport/kakao.js
@@ -10,7 +10,8 @@ passport.use(
   new KakaoStrategy(
     {
       clientID: KAKAO_CLIENT_ID,
-      callbackURL: "http://localhost:5000/auth/kakao/callback", // joopjoop uri 사용시 KAKAO_URL 사용
+      callbackURL: "http://localhost:5000/auth/kakao/callback", // local 사용할 때 사용되는 callbackURL
+      // callbackURL: KAKAO_URL, // 배포시 사용되는 callbackURL
     },
     async (accessToken, refreshToken, profile, done) => {
       console.log(profile);
@@ -19,12 +20,11 @@ passport.use(
         const exUser = await User.findOne({
           oAuthId: profile.id,
         });
-        // 이미 가입된 프로필이면 성공
-        // console.log(exUser);
+
         if (exUser) {
           done(null, exUser); // 로그인 인증 완료
         } else {
-          // 가입되지 않는 유저면 회원가입 시키고 로그인을 시킨다
+          // 가입되지 않는 유저면 회원가입 후 로그인을 시킨다
           const newUser = await new User({
             oAuthId: profile.id,
             nickname: profile.displayName,
