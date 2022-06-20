96a47ea3db52f42eb94ba1b2c4e971240d7fc866
meanjeful <meanjeful@gmail.com>
2022-06-20 18:05:01 +0900

    fix kakao map pin url2

--- client/src/components/container_createmap.jsx
+++ client/src/components/container_createmap.jsx
@@ -20,19 +20,8 @@ const CreateMapContainer = (props) => {
     };
     const map = new kakao.maps.Map(container, options);
 
-    const imageSrc =
-      (process.env.REACT_APP_DEPLOYSERVER_URL ||
-        process.env.REACT_APP_LOCALSERVER_URL) + "/uploads/kakaomappin.png";
-    // ("http://pds27.egloos.com/pds/202206/20/91/c0249191_62af73b1c1ed1.png"); // 마커이미지의 주소입니다
-    console.log(imageSrc);
-    const imageSize = new kakao.maps.Size(30, 43);
-
-    // 마커의 이미지정보를 가지고 있는 마커이미지를 생성합니다
-    const markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize);
-
     const marker = new kakao.maps.Marker({
       position: map.getCenter(),
-      image: markerImage,
     });
     marker.setMap(map);
 
